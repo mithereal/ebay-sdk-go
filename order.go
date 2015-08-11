0ef98ab7b40b30570bcefdc250216f5cc043e47c
Jason Clark <mithereal@gmail.com>
2015-08-10 21:19:51 -0700

    added to struct

--- order.go
+++ order.go
@@ -1,37 +1,37 @@
 package ebay
 
 
-GetOrdersRequest struct {
+type GetOrdersRequest struct {
 	
-		RequesterCredentials = struct {
-		eBayAuthToken
+	type	RequesterCredentials = struct {
+		eBayAuthToken string
 		}
 		
-		CreateTimeFrom  
-		CreateTimeTo  
-		IncludeFinalValueFee           
-		ListingType        
-		ModTimeFrom   
-		ModTimeTo
-		NumberOfDays  
-		OrderRole  
-		OrderStatus  
-		SortingOrder  
-		DetailLevel
-		ErrorLanguage
-		MessageID
-		OutputSelector
-		Version
-		WarningLevel
+		CreateTimeFrom   string
+		CreateTimeTo   string
+		IncludeFinalValueFee    string        
+		ListingType         string
+		ModTimeFrom    string
+		ModTimeTo string
+		NumberOfDays   string
+		OrderRole   string
+		OrderStatus   string
+		SortingOrder   string
+		DetailLevel string
+		ErrorLanguage string
+		MessageID string
+		OutputSelector string
+		Version string
+		WarningLevel string
 		
 
-		Pagination  = struct {
-		EntriesPerPage
-		PageNumber
+	type	Pagination  = struct {
+		EntriesPerPage string
+		PageNumber string
 		}
 		
-		OrderIDArray = struct {
-		OrderID
+	type	OrderIDArray = struct {
+		OrderID string
 		}	
 		
 	} 
